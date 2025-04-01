23ff8a95d75fb09c9b6d078d08d7e8022bb8ba7e
Julien <spaceracer79083@gmail.com>
2025-04-01 15:40:31 +0800

    离院办理模块初始化

---
+++ hospital-discharge-service/src/test/java/com/julien/hospitaldischargeservice/HospitalDischargeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julien.hospitaldischargeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalDischargeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
