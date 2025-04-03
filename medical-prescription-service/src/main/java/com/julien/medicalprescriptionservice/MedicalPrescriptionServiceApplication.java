20e760e52fdf2407327d0427ea18fc62b90dfd38
Julien <spaceracer79083@gmail.com>
2025-04-03 10:19:23 +0800

    初始化病历处方模块。

---
+++ medical-prescription-service/src/main/java/com/julien/medicalprescriptionservice/MedicalPrescriptionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.julien.medicalprescriptionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalPrescriptionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalPrescriptionServiceApplication.class, args);
+    }
+
+}
